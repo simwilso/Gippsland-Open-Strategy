fb494da454aeea591c2d5dc8d3e6cbfc9ecc0ee3
simwilso <simon@decentralize.global>
2025-09-19 11:00:19 +1000

    Deploy site with updated intro.md showing 2025 content

--- site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/simwilso/Code/Gippsland_Open_Roadmap/site/node_modules/infima/dist/css/default/default.css"),
-  require("/home/simwilso/Code/Gippsland_Open_Roadmap/site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/simwilso/Code/Gippsland_Open_Roadmap/site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/simwilso/Code/Gippsland_Open_Roadmap/site/src/css/custom.css"),
-];
