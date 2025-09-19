0569ad6e747be1773469b21336fff60ad7fc7855
simwilso <simon@decentralize.global>
2025-09-19 16:43:00 +1000

    Replace introvideo.mp4 with new intro2.mp4 on homepage

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -25,7 +25,7 @@ function HomepageHeader() {
             className={styles.heroVideo}
             poster={useBaseUrl('/img/welcome-video-thumbnail.jpg')}
           >
-            <source src={useBaseUrl('/video/introvideo.mp4')} type="video/mp4" />
+            <source src={useBaseUrl('/video/intro2.mp4')} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
